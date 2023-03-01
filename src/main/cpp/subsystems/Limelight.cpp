ac6b6e4c76e2dde1d1cad15b80185d0ddc912542
Bill <brussell@bishopdubourg.org>
2023-02-28 18:58:47 -0600

    limelight

--- src/main/cpp/subsystems/Limelight.cpp
+++ src/main/cpp/subsystems/Limelight.cpp
@@ -11,6 +11,10 @@ Limelight::Limelight(){
 
 void Limelight::Periodic() {
     // Put code here to be run every loop
+        frc::SmartDashboard::PutNumber("Limelight Target Area", m_targetArea);
+    frc::SmartDashboard::PutNumber("Limelight Horizontal Offset", m_targetHorizontalOffset);
+    frc::SmartDashboard::PutNumber("Limelight Target Valid", m_targetValid);
+    frc::SmartDashboard::PutNumber("Limelight Vertical Offset", m_targetVerticalOffset);
 }
 
 void Limelight::SimulationPeriodic() {
